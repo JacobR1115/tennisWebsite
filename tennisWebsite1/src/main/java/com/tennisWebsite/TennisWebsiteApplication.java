fd831cc6e4e49d255a7bdb338447f5815efbbd1a
JacobR1115 <123122021+JacobR1115@users.noreply.github.com>
2023-11-06 15:40:59 -0600

    Update TennisWebsiteApplication.java

--- tennisWebsite1/src/main/java/com/tennisWebsite/TennisWebsiteApplication.java
+++ tennisWebsite1/src/main/java/com/tennisWebsite/TennisWebsiteApplication.java
@@ -2,12 +2,19 @@ package com.tennisWebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class TennisWebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TennisWebsiteApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
